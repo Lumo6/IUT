bf24fa4e7821c24410b60d8a521226d5b2f6ae2f
Lucas Vial <160601237+Lumo6@users.noreply.github.com>
2024-10-21 14:40:56 +0200

    Delete 1A/PHP directory

--- 1A/PHP/Connexion/Connexion_base.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Connexion à la base de données
-$bdd = new PDO('mysql:host=localhost;dbname=Infos_connexion','root','');
-$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
